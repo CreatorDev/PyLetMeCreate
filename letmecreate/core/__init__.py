7ef96a73eb786a144a7fde73bd5bec1918b2ea5a
Francois Berder <fberder@outlook.fr>
2016-08-05 18:01:04 +0100

    adc: add python wrapper
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/core/__init__.py
+++ letmecreate/core/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['led', 'switch', 'gpio', 'gpio_monitor', 'common']
+__all__ = ['led', 'switch', 'gpio', 'gpio_monitor', 'common', 'adc']
---
+++ letmecreate/core/adc.py
@@ -0,0 +1,11 @@
+import ctypes
+
+_lib = ctypes.CDLL('libletmecreate_core.so')
+
+
+def get_value(mikrobus_index):
+    value = ctypes.c_float(0)
+    ret = _lib.adc_get_value(mikrobus_index, ctypes.byref(value))
+    if ret < 0:
+        raise Exception("adc get value failed")
+    return value.value
