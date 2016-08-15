8e120f58a51c58d1d753bcb42eab03b518e0f296
Francois Berder <fberder@outlook.fr>
2016-08-15 22:03:26 +0100

    joystick: add docstring
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/joystick.py
+++ letmecreate/click/joystick.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Python binding of Joystick wrapper of LetMeCreate library."""
 
 import ctypes
 
@@ -6,6 +7,11 @@ _LIB = ctypes.CDLL('libletmecreate_click.so')
 
 
 def get_x():
+    """Returns the X position of the joystick.
+
+    Note: An exception is thrown if it fails to read the X position from the
+    chip.
+    """
     pos_x = ctypes.c_int8(0)
     ret = _LIB.joystick_click_get_x(ctypes.byref(pos_x))
     if ret < 0:
@@ -14,6 +20,11 @@ def get_x():
 
 
 def get_y():
+    """Returns the Y position of the joystick.
+
+    Note: An exception is thrown if it fails to read the Y position from the
+    chip.
+    """
     pos_y = ctypes.c_int8(0)
     ret = _LIB.joystick_click_get_y(ctypes.byref(pos_y))
     if ret < 0:
@@ -22,6 +33,11 @@ def get_y():
 
 
 def get_position():
+    """Returns the X position of the joystick.
+
+    Note: An exception is thrown if it fails to read the position from the
+    chip.
+    """
     pos_x = ctypes.c_int8(0)
     pos_y = ctypes.c_int8(0)
     ret = _LIB.joystick_click_get_position(ctypes.byref(pos_x),
