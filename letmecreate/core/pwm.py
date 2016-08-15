b7eeaafaf9484e2fee213ed4bea08be82c727857
Francois Berder <fberder@outlook.fr>
2016-08-15 22:02:01 +0100

    pwm: fix pep8 error
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/core/pwm.py
+++ letmecreate/core/pwm.py
@@ -2,70 +2,70 @@
 
 import ctypes
 
-_lib = ctypes.CDLL('libletmecreate_core.so')
+_LIB = ctypes.CDLL('libletmecreate_core.so')
 
 
 def init(mikrobus_index):
-    ret = _lib.pwm_init(mikrobus_index)
+    ret = _LIB.pwm_init(mikrobus_index)
     if ret < 0:
         raise Exception("pwm init failed")
 
 
 def enable(mikrobus_index):
-    ret = _lib.pwm_enable(mikrobus_index)
+    ret = _LIB.pwm_enable(mikrobus_index)
     if ret < 0:
         raise Exception("pwm enable failed")
 
 
 def set_duty_cycle(mikrobus_index, percentage):
-    ret = _lib.pwm_set_duty_cycle(mikrobus_index, percentage)
+    ret = _LIB.pwm_set_duty_cycle(mikrobus_index, percentage)
     if ret < 0:
         raise Exception("pwm set duty cycle failed")
 
 
 def get_duty_cycle(mikrobus_index):
     percentage = ctypes.c_float(0)
-    ret = _lib.pwm_get_duty_cycle(mikrobus_index, ctypes.byref(percentage))
+    ret = _LIB.pwm_get_duty_cycle(mikrobus_index, ctypes.byref(percentage))
     if ret < 0:
         raise Exception("pwm get duty cycle failed")
     return percentage.value
 
 
 def set_frequency(mikrobus_index, frequency):
-    ret = _lib.pwm_set_frequency(mikrobus_index, frequency)
+    ret = _LIB.pwm_set_frequency(mikrobus_index, frequency)
     if ret < 0:
         raise Exception("pwm set frequency failed")
 
 
 def get_frequency(mikrobus_index):
     frequency = ctypes.c_float(0)
-    ret = _lib.gpio_get_frequency(mikrobus_index, ctypes.byref(frequency))
+    ret = _LIB.gpio_get_frequency(mikrobus_index, ctypes.byref(frequency))
     if ret < 0:
         raise Exception("pwm get frequency failed")
     return frequency.value
 
 
 def set_period(mikrobus_index, period):
-    ret = _lib.pwm_set_period(mikrobus_index, period)
+    ret = _LIB.pwm_set_period(mikrobus_index, period)
     if ret < 0:
         raise Exception("pwm set period failed")
 
 
 def get_period(mikrobus_index):
     period = ctypes.c_uint32(0)
-    ret = _lib.pwm_get_frequency(mikrobus_index, ctypes.byref(period))
+    ret = _LIB.pwm_get_frequency(mikrobus_index, ctypes.byref(period))
     if ret < 0:
         raise Exception("pwm get period failed")
     return period.value
 
 
 def disable(mikrobus_index):
-    ret = _lib.pwm_disable(mikrobus_index)
+    ret = _LIB.pwm_disable(mikrobus_index)
     if ret < 0:
         raise Exception("pwm disable failed")
 
 
 def release(mikrobus_index):
-    ret = _lib.pwm_release(mikrobus_index)
+    ret = _LIB.pwm_release(mikrobus_index)
     if ret < 0:
         raise Exception("pwm release failed")
