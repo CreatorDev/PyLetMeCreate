b303feb0378ad3eb653b7de923c6fb458f9a0845
Francois Berder <fberder@outlook.fr>
2016-08-15 22:01:12 +0100

    gpio_monitor: add documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/core/gpio_monitor.py
+++ letmecreate/core/gpio_monitor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Python binding of gpio_monitor wrapper of LetMeCreate library."""
 
 import ctypes
 
@@ -13,6 +14,10 @@ GPIO_EDGE = GPIO_RAISING | GPIO_FALLING
 
 
 def init():
+    """Initialise gpio_monitor.
+
+    Note: If an error occurs during the initialisation, an exception is thrown.
+    """
     global _CALLBACKS
     ret = _LIB.gpio_monitor_init()
     if ret < 0:
@@ -21,6 +26,17 @@ def init():
 
 
 def add_callback(gpio_pin, event_mask, callback):
+    """Attach a callback to a GPIO. Returns the callback ID.
+
+    gpio_pin: Index of the GPIO. Constants are defined in letmecreate.core.gpio.
+
+    event_mask: should be 1 (GPIO_RAISING), 2 (GPIO_FALLING) or 3 (GPIO_EDGE).
+
+    callback: Function to call
+
+    Note: The GPIO must be configured as an input before calling this function.
+    An exception is thrown if the callback cannot be attached to the GPIO.
+    """
     ptr = _CALLBACK_TYPE(callback)
     ret = _LIB.gpio_monitor_add_callback(gpio_pin, event_mask, ptr)
     if ret < 0:
@@ -29,6 +45,13 @@ def add_callback(gpio_pin, event_mask, callback):
 
 
 def remove_callback(callback_id):
+    """Detach callback from GPIO.
+
+    callback_id: ID of the callback to detach. Must be a positive integer,
+    returned from a previous call to add_callback.
+
+    Note: An exception is thrown if the callback cannot be removed.
+    """
     ret = _LIB.gpio_monitor_remove_callback(callback_id)
     if ret < 0:
         raise Exception("gpio monitor remove callback failed")
@@ -36,6 +59,10 @@ def remove_callback(callback_id):
 
 
 def release():
+    """Release gpio_monitor.
+
+    Deletes all callbacks currently attached to GPIO's.
+    """
     ret = _LIB.gpio_monitor_release()
     if ret < 0:
         raise Exception("gpio monitor release failed")
