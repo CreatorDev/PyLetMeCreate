0c697f92248cc025b7f354f97b297c091677e3e8
Francois Berder <fberder@outlook.fr>
2016-08-15 22:04:59 +0100

    thermo3: add docstring
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/thermo3.py
+++ letmecreate/click/thermo3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Python binding of Thermo3 Click wrapper of LetMeCreate library.
 
+You must configure and select the right I2C bus before using any of these
+functions.
+"""
 import ctypes
 
 _LIB = ctypes.CDLL('libletmecreate_click.so')
@@ -8,12 +12,25 @@ _CALLBACKS = [None, None]
 
 
 def enable(add_bit):
+    """Enable Thermo3 Click.
+
+    add_bit: A jumper is present on the Thermo3 Click which modifies the I2C
+    slave address of the chip. Set this variable to 0 or 1 according to the
+    state of the jumper.
+
+    Note: An exception is thrown if it fails to enable the Thermo3 Click.
+    """
     ret = _LIB.thermo3_click_enable(add_bit)
     if ret < 0:
         raise Exception("thermo3 click enable failed")
 
 
 def get_temperature():
+    """Returns temperature measure.
+
+    Note: An exception is thrown if it fails to read the temperature from the
+    chip.
+    """
     temperature = ctypes.c_float(0)
     ret = _LIB.thermo3_click_get_temperature(ctypes.byref(temperature))
     if ret < 0:
@@ -22,6 +39,16 @@ def get_temperature():
 
 
 def set_alarm(mikrobus_index, threshold, callback):
+    """Triggers a callback if the temperature exceeds a threshold.
+
+    mikrobus_index: must be 0 (MIKROBUS_1) or 1 (MIKROBUS_2)
+
+    threshold: Floating point variable.
+
+    callback: Function to call if temperature is over threshold.
+
+    Note: An exception is thrown if it fails to set the alarm.
+    """
     ptr = _CALLBACK_TYPE(callback)
     ret = _LIB.thermo3_click_set_alarm(mikrobus_index, threshold, ptr)
     if ret < 0:
@@ -30,6 +57,10 @@ def set_alarm(mikrobus_index, threshold, callback):
 
 
 def disable():
+    """Disable Thermo3 Click.
+
+    Note: An exception is thrown if it fails to disable the chip.
+    """
     ret = _LIB.thermo3_click_disable()
     if ret < 0:
         raise Exception("thermo3 click disable failed")
