908881f4d9a98a6d64e73c56636e09825b7d02bd
Francois Berder <fberder@outlook.fr>
2016-08-15 22:05:04 +0100

    add module docstring
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/__init__.py
+++ letmecreate/click/__init__.py
@@ -1,2 +1,15 @@
+"""Python binding of the click part of the LetMeCreate library.
+
+It defines binding for the following click boards:
+  - Thermo3
+  - Proximity
+  - Accel
+  - Relay2
+  - Motion
+  - 8x8R (LED matrix)
+  - Joystick
+  - Color
+"""
+
 __all__ = ['thermo3', 'proximity', 'accel', 'relay2', 'motion', 'led_matrix',
            'joystick', 'color']
