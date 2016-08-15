9694833b9feb41fd6dca87c08e8d8496059270c9
Francois Berder <fberder@outlook.fr>
2016-08-15 22:00:56 +0100

    adc: fix pep8 error
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/core/adc.py
+++ letmecreate/core/adc.py
@@ -12,7 +12,7 @@ __maintainer__ = "Francois Berder"
 __email__ = "fberder@outlook.fr"
 __status__ = "Development"
 
-_lib = ctypes.CDLL('libletmecreate_core.so')
+_LIB = ctypes.CDLL('libletmecreate_core.so')
 
 
 def get_value(mikrobus_index):
@@ -24,7 +24,7 @@ def get_value(mikrobus_index):
     letmecreate.core.common.
     """
     value = ctypes.c_float(0)
-    ret = _lib.adc_get_value(mikrobus_index, ctypes.byref(value))
+    ret = _LIB.adc_get_value(mikrobus_index, ctypes.byref(value))
     if ret < 0:
         raise Exception("adc get value failed")
     return value.value
