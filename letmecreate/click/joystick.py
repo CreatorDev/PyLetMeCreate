10fd5faba32bfc31aa2e0fad485d6de472be8af2
Francois Berder <fberder@outlook.fr>
2016-08-15 22:03:22 +0100

    joystick: fix pep8 errors
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/joystick.py
+++ letmecreate/click/joystick.py
@@ -2,29 +2,30 @@
 
 import ctypes
 
-_lib = ctypes.CDLL('libletmecreate_click.so')
+_LIB = ctypes.CDLL('libletmecreate_click.so')
 
 
 def get_x():
-    x = ctypes.c_int8(0)
-    ret = _lib.joystick_click_get_x(ctypes.byref(x))
+    pos_x = ctypes.c_int8(0)
+    ret = _LIB.joystick_click_get_x(ctypes.byref(pos_x))
     if ret < 0:
         raise Exception("joystick click get x failed")
-    return x.value
+    return pos_x.value
 
 
 def get_y():
-    y = ctypes.c_int8(0)
-    ret = _lib.joystick_click_get_y(ctypes.byref(y))
+    pos_y = ctypes.c_int8(0)
+    ret = _LIB.joystick_click_get_y(ctypes.byref(pos_y))
     if ret < 0:
         raise Exception("joystick click get y failed")
-    return y.value
+    return pos_y.value
 
 
 def get_position():
-    x = ctypes.c_int8(0)
-    y = ctypes.c_int8(0)
-    ret = _lib.joystick_click_get_position(ctypes.byref(x), ctypes.byref(y))
+    pos_x = ctypes.c_int8(0)
+    pos_y = ctypes.c_int8(0)
+    ret = _LIB.joystick_click_get_position(ctypes.byref(pos_x),
+                                           ctypes.byref(pos_y))
     if ret < 0:
         raise Exception("joystick click get position failed")
-    return (x.value, y.value)
+    return (pos_x.value, pos_y.value)
