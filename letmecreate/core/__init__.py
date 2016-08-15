e1917aa4bac587d7a9d79a9d16df5b3e6a0ffc28
Francois Berder <fberder@outlook.fr>
2016-08-15 22:03:01 +0100

    core: add module documentation
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/core/__init__.py
+++ letmecreate/core/__init__.py
@@ -1,2 +1,15 @@
+"""Python binding of the core part of the LetMeCreate library.
+
+It defines binding for the following interface:
+  - adc
+  - gpio and gpio_monitor
+  - switch
+  - UART
+  - I2C
+  - SPI
+  - PWM
+  - led
+"""
+
 __all__ = ['led', 'switch', 'gpio', 'gpio_monitor', 'common', 'adc', 'pwm',
            'uart', 'i2c', 'spi']
