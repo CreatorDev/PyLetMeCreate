6da8b989c9e2a27a2944e6f80e14e6147107236d
Francois Berder <fberder@outlook.fr>
2016-08-15 22:04:04 +0100

    proximity: fix pep8 error
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/proximity.py
+++ letmecreate/click/proximity.py
@@ -2,24 +2,24 @@
 
 import ctypes
 
-_lib = ctypes.CDLL('libletmecreate_click.so')
+_LIB = ctypes.CDLL('libletmecreate_click.so')
 
 
 def enable():
-    ret = _lib.proximity_click_enable()
+    ret = _LIB.proximity_click_enable()
     if ret < 0:
         raise Exception("proximity click enable failed")
 
 
 def get_measure():
     measure = ctypes.c_uint16(0)
-    ret = _lib.proximity_click_get_measure(ctypes.byref(measure))
+    ret = _LIB.proximity_click_get_measure(ctypes.byref(measure))
     if ret < 0:
         raise Exception("proximity click get measure failed")
     return measure.value
 
 
 def disable():
-    ret = _lib.proximity_click_disable()
+    ret = _LIB.proximity_click_disable()
     if ret < 0:
         raise Exception("proximity click disable failed")
