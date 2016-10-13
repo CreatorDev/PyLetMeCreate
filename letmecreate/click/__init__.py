b83a09f437449ce29a80a4e5c8b9559e7da85112
Francois Berder <fberder@outlook.fr>
2016-10-13 20:38:50 +0100

    order in alphabetical order click board
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/__init__.py
+++ letmecreate/click/__init__.py
@@ -1,26 +1,26 @@
 """Python binding of the click part of the LetMeCreate library.
 
 It defines binding for the following click boards:
-  - Thermo3
-  - Proximity
-  - Accel
-  - Relay2
-  - Motion
-  - 8x8R (LED matrix)
-  - Joystick
-  - Color
   - 7Seg
-  - IR distance
-  - Bargraph
-  - Relay (partial support: only relay 1 working)
-  - IR eclipse
-  - Color2
+  - 8x8R (LED matrix)
+  - Accel
   - ADC
   - Alcohol
+  - Bargraph
+  - Color
+  - Color2
   - Eve
+  - IR distance
+  - IR eclipse
+  - Joystick
+  - Motion
+  - Proximity
+  - Relay (partial support: only relay 1 working)
+  - Relay2
   - Relay4
+  - Thermo3
 """
 
-__all__ = ['thermo3', 'proximity', 'accel', 'relay2', 'motion', 'led_matrix',
-           'joystick', 'color', 'seven_seg', 'ir_distance', 'bargraph',
-           'relay', 'ir_eclipse', 'color2', 'adc', 'alcohol', 'eve', 'relay4']
+__all__ = ['seven_seg', 'led_matrix', 'accel', 'adc', 'alcohol', 'bargraph',
+           'color', 'color2', 'eve', 'ir_distance', 'ir_eclipse', 'joystick',
+           'motion', 'proximity', 'relay', 'relay2', 'relay4', 'thermo3']
