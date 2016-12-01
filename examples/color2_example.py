6281c97372b182592fddd8ca7013f4b01ee3220d
Francois Berder <fberder@outlook.fr>
2016-12-01 22:08:40 +0000

    color2: add example
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ examples/color2_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from letmecreate.core import i2c
+from letmecreate.click import color2
+
+
+i2c.init()
+color2.enable()
+
+measure = color2.get_color()
+color2.disable()
+i2c.release()
+
+print('red  : {}'.format(measure[0]))
+print('green: {}'.format(measure[1]))
+print('blue : {}'.format(measure[2]))
