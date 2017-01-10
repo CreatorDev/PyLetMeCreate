7a1f94e05a0be3289d9faff66e407c5f92bc3067
Francois Berder <fberder@outlook.fr>
2017-01-10 19:54:02 +0000

    click/__init__: add missing wrappers
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- letmecreate/click/__init__.py
+++ letmecreate/click/__init__.py
@@ -12,21 +12,26 @@ It defines binding for the following click boards:
   - Color
   - Color2
   - Eve
+  - Fan
   - GYRO
   - IR distance
   - IR eclipse
   - Joystick
+  - Light
   - LIN Hall
   - Motion
+  - Oled
   - Proximity
   - Relay (partial support: only relay 1 working)
   - Relay2
   - Relay4
+  - RTC
   - Thermo3
   - UNI Hall
 """
 
 __all__ = ['seven_seg', 'led_matrix', 'accel', 'adc', 'air_quality', 'alcohol',
-           'bargraph', 'CO', 'color', 'color2', 'eve', 'gyro', 'ir_distance',
-           'ir_eclipse', 'joystick', 'lin_hall', 'motion', 'proximity', 'relay',
-           'relay2', 'relay4', 'rtc', 'thermo3', 'uni_hall']
+           'bargraph', 'CO', 'color', 'color2', 'eve', 'fan', 'gyro',
+           'ir_distance', 'ir_eclipse', 'joystick', 'light', 'lin_hall',
+           'motion', 'oled', 'proximity', 'relay', 'relay2', 'relay4', 'rtc',
+           'thermo3', 'uni_hall']
