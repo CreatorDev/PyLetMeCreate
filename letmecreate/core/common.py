7980a0ecec34564829a13bda0271cadb9ebaad3a
Francois Berder <francois.berder@imgtec.com>
2017-02-03 17:05:35 +0000

    core_common: Add MIKROBUS_COUNT and check_valid_mikrobus
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- letmecreate/core/common.py
+++ letmecreate/core/common.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 """Define constants for mikrobus index."""
 
+import ctypes
+
+_LIB = ctypes.CDLL('libletmecreate_core.so')
+
 MIKROBUS_1 = 0
 MIKROBUS_2 = 1
+MIKROBUS_COUNT = 2
+
+
+def check_valid_mikrobus(mikrobus_index):
+    """Check if mikrobus exists
+        Returns 0 if succesful, -1 otherwise.
+    """
+    return _LIB.check_valid_mikrobus(mikrobus_index)
