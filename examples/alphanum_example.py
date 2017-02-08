afb22c19277a221e011aa89c65e4716bf1304c41
Francois Berder <francois.berder@imgtec.com>
2017-02-08 11:53:33 +0000

    alphanum: Synchronize example with LetMeCreate
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- examples/alphanum_example.py
+++ examples/alphanum_example.py
@@ -2,10 +2,8 @@
 """This example shows how to use the Alphanum Click wrapper of the LetMeCreate
 to display characters.
 
-It displays "Ci" using both displays by enabling them one after the other at
-100Hz to give the illusion that both characters are displayed at the same
-time. The user has to interrupt the program to exit it by pressing Ctrl+C.
- *
+It displays "Ci" during 5 seconds
+
 The Alphanum Click must be inserted in Mikrobus 1 before running this
 program.
 """
@@ -13,15 +11,14 @@ program.
 from letmecreate.core.common import MIKROBUS_1
 from letmecreate.core import spi
 from letmecreate.click import alphanum
-import time
+from time import sleep
 
 
 spi.init()
 alphanum.init(MIKROBUS_1)
 alphanum.write('C', 'i')
 
-while True:
-    alphanum.select_left_display()
-    time.sleep(0.01)
-    alphanum.select_right_display()
-    time.sleep(0.01)
+sleep(5)
+
+alphanum.release()
+spi.release()
